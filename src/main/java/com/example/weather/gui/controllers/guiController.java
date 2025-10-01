182f89d82dc033e087cda3364b67bfed86d72d05
spamntuna <pamintuane16609@gotritons.uog.edu>
2025-10-01 22:25:39 +1000

    [ADDED] gui.fxml -created base GUI
    guiController.java
    -defined text fields & buttons

---
+++ src/main/java/com/example/weather/gui/controllers/guiController.java
@@ -0,0 +1,20 @@
+package com.example.weather.gui.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class guiController {
+    @FXML
+    private TextField txtTo;
+
+    @FXML
+    private TextField txtSubject;
+
+    @FXML
+    private TextField txtBody;
+
+    @FXML
+    private Button btnSend;
+
+}
