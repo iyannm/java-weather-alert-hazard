a24cf65ac95b941decf5742406c9444a492562a3
iyannm <luguei16526@gotritons.uog.edu>
2025-10-01 23:08:15 +1000

    running email

--- src/main/java/com/example/weather/app/MainApp.java
+++ src/main/java/com/example/weather/app/MainApp.java
@@ -1,7 +1,8 @@
-package com.example.weather;
+package com.example.weather.app;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -9,8 +10,10 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/email.fxml"));
-        Scene scene = new Scene(loader.load(), 600, 400);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/gui.fxml"));
+        Parent root = loader.load();  // load FXML once
+
+        Scene scene = new Scene(root, 600, 400); // use the already loaded root
         primaryStage.setTitle("WAH Email Project");
         primaryStage.setScene(scene);
         primaryStage.show();
