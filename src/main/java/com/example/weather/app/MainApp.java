6fad478158d61ce3b779a6d3271ef035d3780c3e
iyannm <luguei16526@gotritons.uog.edu>
2025-10-01 23:26:59 +1000

    test comment

--- src/main/java/com/example/weather/app/MainApp.java
+++ src/main/java/com/example/weather/app/MainApp.java
@@ -18,7 +18,7 @@ public class MainApp extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
+//comment test
     public static void main(String[] args) {
         launch(args);
     }
