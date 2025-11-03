3869baca3e384d8ffe9a3f6eb08f556c8c54df8a
iyannm <luguei16526@gotritons.uog.edu>
2025-11-03 20:49:38 +1000

    all updates

--- src/main/java/com/example/emailsender/app/MainApp.java
+++ src/main/java/com/example/emailsender/app/MainApp.java
@@ -10,7 +10,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/startpage.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/gui.fxml"));
         Parent root = loader.load();  // load FXML once
 
         Scene scene = new Scene(root, 800, 600);
